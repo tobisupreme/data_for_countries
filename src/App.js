969284f65e3632ebee3597d6bc654b42be64c943
Tobi Balogun <tobi.balo@gmail.com>
2022-09-16 02:20:39 +0100

    fix: limit query displayed to user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
+const DisplaySingleList = ({ country }) => {
+  return (
+    <div>
+      {country.name.common}
+    </div>
+  )
+}
+
+const DisplayWarning = () => {
+
+  return <div>Too many matches, specify another filter</div>
+}
+
+const DisplayHandler = ({countries}) => {
+  if (countries.length > 10) {
+    return <DisplayWarning/>
+  }
+  
+  return (
+    <>
+      {countries.map((country) => {
+        return <DisplaySingleList key={country.cca2} country={country}/>
+      })}
+    </>
+  )
+}
+
 const Filter = ({onChange}) => {
   return (
     <div>
@@ -25,7 +52,6 @@ const App = () => {
 
   const filterListing = (e) => {
     const len = e.target.value.length
-    console.log(len)
     if (len === 0) {
       searchQuery.isEmpty = true
     } else {
@@ -36,19 +62,21 @@ const App = () => {
     setSearchQuery({ ...searchQuery, query: updateQuery })
   }
 
-  const dataToShow = searchQuery.isEmpty
+  const setDataToShow = () => {
+    return searchQuery.isEmpty
     ? []
     : countries.filter((country) => {
         return country.name.common.toLowerCase().includes(searchQuery.query.toLowerCase())
       })
+  }
+
+  let dataToShow = setDataToShow()
 
   return (
     <div>
       <h2>Data for countries</h2>
       <Filter onChange={filterListing} />
-      {dataToShow.map((country) => {
-        return <div>{country.name.common}</div>
-      })}
+      <DisplayHandler countries={dataToShow} />
     </div>
   )
 }
