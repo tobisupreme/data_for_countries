f168c47f45ec25444bb31beada7da3c540b46523
Tobi Balogun <tobi.balo@gmail.com>
2022-09-16 01:53:40 +0100

    feat: implement basic sorting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
+const Filter = ({onChange}) => {
+  return (
+    <div>
+      find countries <input onChange={onChange} />
+    </div>
+  )
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [searchQuery, setSearchQuery] = useState({isEmpty: true, query: ''})
 
   const hook = () => {
     axios
@@ -14,16 +23,32 @@ const App = () => {
 
   useEffect(hook, [])
 
-  console.log(countries)
+  const filterListing = (e) => {
+    const len = e.target.value.length
+    console.log(len)
+    if (len === 0) {
+      searchQuery.isEmpty = true
+    } else {
+      searchQuery.isEmpty = false
+    }
+
+    const updateQuery = e.target.value
+    setSearchQuery({ ...searchQuery, query: updateQuery })
+  }
+
+  const dataToShow = searchQuery.isEmpty
+    ? []
+    : countries.filter((country) => {
+        return country.name.common.toLowerCase().includes(searchQuery.query.toLowerCase())
+      })
 
   return (
     <div>
       <h2>Data for countries</h2>
-      <ul>
-      {countries.map(country => {
-          return <li>{country.name.common}</li>
-        })}
-      </ul>
+      <Filter onChange={filterListing} />
+      {dataToShow.map((country) => {
+        return <div>{country.name.common}</div>
+      })}
     </div>
   )
 }
