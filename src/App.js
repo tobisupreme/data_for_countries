c727b6606cf47295c1c9567b90399f9552e61d20
Tobi Balogun <tobi.balo@gmail.com>
2022-09-16 01:38:40 +0100

    feat: get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+
+  const hook = () => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(data => {
+        setCountries(data.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log(countries)
+
   return (
     <div>
       <h2>Data for countries</h2>
+      <ul>
+      {countries.map(country => {
+          return <li>{country.name.common}</li>
+        })}
+      </ul>
     </div>
   )
 }
